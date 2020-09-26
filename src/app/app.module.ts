34454af36ce45257648730b2645e777ba81b884a
Radwa <radwayassin26@gmail.com>
2020-09-26 04:47:51 +0200

    change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { OwlModule } from 'ngx-owl-carousel';  
 import { AppComponent } from './app.component';
 import { FooterComponent } from './SharedCom/footer/footer.component';
 import { HeaderComponent } from './SharedCom/header/header.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    HeaderComponent
-  ],
+    HeaderComponent,
+    HomeComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    OwlModule
   ],
   providers: [],
   bootstrap: [AppComponent]
